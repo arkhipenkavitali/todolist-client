be69ce90d3b396e1932c49235c46ec9fa5c48a66
vitali <arkhipenka.v@brainrocket.com>
2025-06-03 16:43:35 +0300

    add modal esc logic

--- src/components/ConfirmModal/ConfirmModal.tsx
+++ src/components/ConfirmModal/ConfirmModal.tsx
@@ -1,6 +1,7 @@
-import {useDispatch} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import {closeModal} from "../../store/slices/modalSlice.ts";
-import React from "react";
+import React, {useEffect} from "react";
+import {RootState} from "../../store/store.ts";
 
 interface ConfirmModalProps {
 	title: string;
@@ -9,6 +10,22 @@ interface ConfirmModalProps {
 
 const ConfirmModal: React.FC<ConfirmModalProps> = ({onConfirm, title}) => {
 	const dispatch = useDispatch();
+	const modalContent = useSelector((state: RootState) => state.modal.content);
+	
+	useEffect(() => {
+		const handleModalCloseByEsc = (e: KeyboardEvent) => {
+			if(e.key === 'Escape'){
+				dispatch(closeModal());
+			}
+		}
+		if(modalContent){
+			document.addEventListener("keydown", handleModalCloseByEsc);
+		}
+		
+		return () => {
+			document.removeEventListener("keydown", handleModalCloseByEsc);
+		}
+	}, [modalContent, dispatch])
 	
 	const handleConfirm = () => {
 		onConfirm();
