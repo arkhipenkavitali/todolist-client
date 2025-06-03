72b37149b68d5f45ac4900a3596027524b7491ba
vitality <qwerty@qwerty.com>
2025-06-03 20:14:55 +0300

    add scss modules

--- src/components/ModalContainer/ModalContainer.tsx
+++ src/components/ModalContainer/ModalContainer.tsx
@@ -1,15 +1,15 @@
 import {useSelector} from "react-redux";
 import {RootState} from "../../store/store.ts";
-import './ModalContainer.css';
+import styles from './ModalContainer.module.scss';
 
 const ModalContainer = () => {
-	const modalContent = useSelector((state: RootState) => state.modal.content);
-	if(!modalContent) return null;
-	return (
-		<div className="modal-backdrop">
-			<div className="modal">{modalContent}</div>
-		</div>
-	);
+    const modalContent = useSelector((state: RootState) => state.modal.content);
+    if (!modalContent) return null;
+    return (
+        <div className={styles['modal-backdrop']}>
+            <div className={styles.modal}>{modalContent}</div>
+        </div>
+    );
 };
 
 export default ModalContainer;
